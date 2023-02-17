3e987410c1a42247a83286e4219febf2d354fefc
Jessica Nash <jessicanashacc@gmail.com>
2023-02-17 23:10:28 +0900

    api is working now, still need to finish search

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import '../App.css';
-import React, { useReducer, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Header } from './Header'
 import { Movie } from './Movie';
 import { Search } from './Search';
@@ -7,94 +7,83 @@ import env from "react-dotenv"
 
 const tmdbKey = env.REACT_APP_API_KEY;
 const MOVIE_API_URL = `https://api.themoviedb.org/3/search/movie?api_key=${tmdbKey}`;
+const query = "Kiki's delivery service"
+// const initialState = {
+//   loading: true,
+//   movies: [],
+//   errorMessage: null
+// };
 
-const initialState = {
-  loading: true,
-  movies: [],
-  errorMessage: null
-};
 
-
-const reducer = (state, action) => {
-  switch (action.type) {
-    case "SEARCH_MOVIES_REQUEST":
-      return {
-        ...state,
-        loading: true,
-        errorMessage: null
-      };
-    case "SEARCH_MOVIES_SUCCESS":
-      return {
-        ...state,
-        loading: false,
-        movies: action.payload
-      };
-    case "SEARCH_MOVIES_FAILURE":
-      return {
-        ...state,
-        loading: false,
-        errorMessage: action.error
-      };
-    default:
-      return state;
-  }
-};
+// const reducer = (state, action) => {
+//   switch (action.type) {
+//     case "SEARCH_MOVIES_REQUEST":
+//       return {
+//         ...state,
+//         loading: true,
+//         errorMessage: null
+//       };
+//     case "SEARCH_MOVIES_SUCCESS":
+//       return {
+//         ...state,
+//         loading: false,
+//         movies: action.payload
+//       };
+//     case "SEARCH_MOVIES_FAILURE":
+//       return {
+//         ...state,
+//         loading: false,
+//         errorMessage: action.error
+//       };
+//     default:
+//       return state;
+//   }
+// };
 
 const App = () => {
-  // const [loading, setLoading] = useState();
-  // const [movies, setMovies] = useState();
-  // const [errorMessage, setErrorMessage] = useState();
+  // renders a ‘loading…’ text when loading is set to true
+  const [loading, setLoading] = useState(true);
 
-  const [state, dispatch] = useReducer(reducer, initialState);
+  //used to handle the movies array that is gotten from the server
+  const [movies, setMovies] = useState([]);
 
-  useEffect(()=> {
-    fetch(MOVIE_API_URL)
-      .then(response => response.json)
+  //used to handle any errors that might occur when making the API request
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  // This hook basically lets you perform side effects in your function components.
+  // Side effects, e.g. data fetching, subscriptions, and manual DOM manipulations.
+  useEffect(() => {
+    fetch(`${MOVIE_API_URL}&query=${query}`)
+      .then(response => response.json())
       .then(jsonResponse => {
-        // setMovies(jsonResponse.Search);
-        // setLoading(false);
-        dispatch({
-          type: "SEARCH_MOVIES_SUCCESS",
-          payload: jsonResponse.Search
-        });
-      })
+        setMovies(jsonResponse.results);
+        setLoading(false);
+      });
   }, []);
 
   const search = searchValue => {
-    // setLoading(true);
-    // setErrorMessage(null);
-    dispatch({
-      type: "SEARCH_MOVIES_REQUEST"
-    });
+    setLoading(true);
+    setErrorMessage(null);
 
     fetch(`${MOVIE_API_URL}&query=${searchValue}`)
-      .then(response => response.json)
+      .then(response => response.json())
       .then(jsonResponse => {
-        if (jsonResponse === 'True') {
-          // setLoading(false);
-          dispatch({
-            type: "SEARCH_MOVIES_SUCCESS",
-            payload: jsonResponse.Search
-          });
+        if (jsonResponse.Response === "True") {
+          setMovies(jsonResponse.results);
+          setLoading(false);
         } else {
-          // setErrorMessage(jsonResponse.Error);
-          // setLoading(false);
-          dispatch({
-            type: "SEARCH_MOVIES_FAILURE",
-            error: jsonResponse.Error
-          });
+          setErrorMessage(jsonResponse.Error);
+          setLoading(false);
         }
-      })
-  }
-
-  const { movies, errorMessage, loading } = state;
+      });
+  };
 
 
   return (
     <div className="App">
-      <Header text="Cinema Seeker"/>
-      <Search search={search}/>
-      <p className="App-intro">Here's some of our favorite movies</p>
+      <Header text="HOOKED" />
+      <Search search={search} />
+      <p className="App-intro">Sharing a few of our favourite movies</p>
       <div className="movies">
         {loading && !errorMessage ? (
           <span>loading...</span>
@@ -102,7 +91,7 @@ const App = () => {
           <div className="errorMessage">{errorMessage}</div>
         ) : (
           movies.map((movie, index) => (
-            <Movie key={`${index}-${movie.Title}`} movie={movie} />
+            <Movie key={`${index}-${movie.title}`} movie={movie} />
           ))
         )}
       </div>
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -7,17 +7,18 @@ const DEFAULT_PLACEHOLDER_IMAGE =
   "https://m.media-amazon.com/images/M/MV5BMTczNTI2ODUwOF5BMl5BanBnXkFtZTcwMTU0NTIzMw@@._V1_SX300.jpg";
 
 export const Movie = ({movie}) => {
-  const poster = movie.Poster === "N/A" ? DEFAULT_PLACEHOLDER_IMAGE : movie.Poster
+  const poster = `https://image.tmdb.org/t/p/original${movie.poster_path}`
   // if the movie's poster's value is set to "N/A" then the const poster is set to the default img,
   // if not, movie.poster is set to poster
 
   return (
     <div className="movie">
-      <h1>{movie.Title}</h1>
+      <h1>{movie.title}</h1>
       <div>
-        <img src={poster} alt={`The movie titled: ${movie.Title}`} />
+        <img src={poster} alt={`The movie titled: ${movie.title}`} className="poster" />
+        {/* <img alt={`The movie titled: ${movie.Title}`} /> */}
       </div>
-      <p>{movie.Year}</p>
+      <p>{movie.release_date}</p>
     </div>
   )
 }
