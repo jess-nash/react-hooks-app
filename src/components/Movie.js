fa15ea165c8998f24c1576a0c62f7e90eb372eee
Jessica Nash <jessicanashacc@gmail.com>
2023-02-19 19:32:51 +0900

    added links to tmdb movie page

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -6,16 +6,17 @@ import React from "react";
 const DEFAULT_PLACEHOLDER_IMAGE =
   "https://m.media-amazon.com/images/M/MV5BMTczNTI2ODUwOF5BMl5BanBnXkFtZTcwMTU0NTIzMw@@._V1_SX300.jpg";
 
-export const Movie = ({movie}) => {
-  const poster = movie.poster_path === null ? DEFAULT_PLACEHOLDER_IMAGE : `https://image.tmdb.org/t/p/original${movie.poster_path}`
+  export const Movie = ({movie}) => {
+    const poster = movie.poster_path === null ? DEFAULT_PLACEHOLDER_IMAGE : `https://image.tmdb.org/t/p/original${movie.poster_path}`
+    const movieUrl = `https://www.themoviedb.org/movie/${movie.id}`
 
-  return (
-    <div className="movie">
-      <h1>{movie.title}</h1>
-      <div>
-        <img src={poster} alt={`The movie titled: ${movie.title}`} className="poster" />
+    return (
+      <div className="movie">
+        <a href={movieUrl} target="_default"><h1>{movie.title}</h1></a>
+        <div>
+          <img src={poster} alt={`The movie titled: ${movie.title}`} className="poster" />
+        </div>
+        <p>{movie.release_date}</p>
       </div>
-      <p>{movie.release_date}</p>
-    </div>
-  )
+    )
 }
